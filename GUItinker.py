f48ecebab11367b1fee3d2ee94a6316fca79ee4a
Ondřej Wünsch <wunsch.ondrej@gmail.com>
2018-06-11 19:30:24 +0200

    tkinter najs

---
+++ GUItinker.py
@@ -0,0 +1,140 @@
+import tkinter as tk
+from functools import partial
+
+
+global game_grid
+global ship_counter
+global pole
+
+ship_counter = 0
+
+pole = []
+
+game_grid =        [[0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0,0,0,0]]
+
+
+#____Colors___________________________________________________________________#
+bg_blue = "#3CAEE5"
+menubuton_activebg = "#94A7B1"
+menubuton_bg = "#BED5E1"
+sea_blue = "#3496C5"
+act_sea_blue = "#2380AC"
+
+#define window
+window = tk.Tk()
+
+#window title
+window.title("best game ever")
+
+#window size
+wd_width = 800
+wd_height = 600
+wd_size =(str(wd_width),"x",str(wd_height))
+window.geometry("".join(wd_size))
+
+def place_ships(x,y):
+    ship_counter = 0
+    if game_grid[x][y] == 0 and ship_counter < 20:
+        game_grid[x][y] = 1
+        pole[x][y].configure(bg = "black")
+        ship_counter += 1
+    
+    if game_grid[x][y] == 1 and ship_counter > 0:
+        game_grid[x][y] = 0
+        pole[x][y].configure(bg = "black")
+        ship_counter -= 1 
+
+
+#generate buton grid with function in them
+def button_grid(function):   
+    for x in range(0,10):
+        row = []
+        for y in range(0,10):
+            tile_x = 0
+            tile_y = 0
+            tile_pad = 1
+            tile_square = 50
+            
+            tile = tk.Button(window,
+                             bd = 0,
+                             bg = sea_blue,
+                             activebackground = act_sea_blue,
+                             command = partial(function, x, y))
+            
+            tile.place(x = tile_x + x * (tile_square + tile_pad),
+                       y = tile_y + y * (tile_square + tile_pad),
+                       width = tile_square,
+                       height = tile_square,
+                       )
+            row.append(tile)
+        pole.append(row)
+
+#_____Main menu_______________________________________________________________#
+def main_menu():
+    #change background
+    window.configure(background = bg_blue)
+    
+    #title = tk.Label(window,
+    #                 text = "LODĚ (woow)")
+    
+    #title.place(x = wd_width/2,
+    #            y = 0)
+    
+    host = tk.Button(window,
+                     text = "Host",
+                     font =("Arial",30),
+                     bd = 0,
+                     bg = menubuton_bg,
+                     activebackground = menubuton_activebg,
+                     command = host_wd(),
+                     )
+    host.place(x = (wd_width/2)-50,
+               y = wd_height/4,
+               width = 100,
+               height = 50,
+               )
+    
+    join = tk.Button(window,
+                     text = "Join",
+                     font =("Arial",30),
+                     bg = menubuton_bg,
+                     bd = 0,
+                     activebackground = menubuton_activebg,
+                     command = join_wd(),
+                     )
+    join.place(x = (wd_width/2)-50,
+               y = 2*wd_height/4,
+               width = 100,
+               height = 50,
+               )
+            
+#_____Host window_____________________________________________________________#
+def host_wd():
+    pass
+
+#_____Join window_____________________________________________________________#
+def join_wd():
+    pass
+
+#_____Place ships_____________________________________________________________#
+def place_wd():
+    #change background
+    window.configure(background = bg_blue)
+    
+    button_grid(place_ships)
+    
+    
+    
+    
+    
+place_wd()
+window.mainloop()
